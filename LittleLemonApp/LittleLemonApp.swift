57d86c5cdd441ee1ee823e5187b79b977b37ee50
quocNV_vn <quocnv155@gmail.com>
2024-08-08 18:55:20 +0900

    init project

---
+++ LittleLemonApp/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonAppApp.swift
+//  LittleLemonApp
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    let persistenceController = PersistenceController.shared
+    
+    var body: some Scene {
+        WindowGroup {
+            Onboarding().environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ LittleLemonApp/Views/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  LittleLemonApp
+//
+
+import SwiftUI
+
+struct Home: View {
+    
+    var body: some View {
+        MainScreen()
+            .navigationBarBackButtonHidden()
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home().environment(\.managedObjectContext, PersistenceController.shared.container.viewContext)
+    }
+}
